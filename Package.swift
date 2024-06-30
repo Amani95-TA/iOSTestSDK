c1ace7d1bf50d36c025215b1b8bf1b67b49fea25
Amani95-TA <atawalbeh@blessedtreeit.com>
2024-06-30 09:53:38 +0300

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "iOSTestSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "iOSTestSDK",
+            targets: ["iOSTestSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "iOSTestSDK",
+            path: "Frameworks/iOSTestSDK.xcframework"
+        )
+    ]
+)
